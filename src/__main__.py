59a8f3e0f8505e3150612aec8d8fe63113f85bdb
shamilbi <shamilbi@gmail.com>
2020-03-06 19:20:43 +0700

    src/__main__.py: +main(), setup.py: chmod 644

--- src/__main__.py
+++ src/__main__.py
@@ -30,27 +30,32 @@ if hasattr(sys, "frozen") and (sys.platform != 'darwin'):
 else:
     config.set_basescript(__file__)
 
-# If cmdline arguments were given, use the "cmdline" frontend.
-if len(sys.argv) > 1:
-    from src.frontends.cmdline import loxodo
-    sys.exit()
 
-# In all other cases, use the "wx" frontend.
-try:
-    import wx
-    assert wx.__version__.startswith('4.0.')
-except AssertionError as e:
-    print('Found incompatible wxPython, the wxWidgets Python bindings: %s' % wx.__version__, file=sys.stderr)
-    print('Falling back to cmdline frontend.', file=sys.stderr)
-    print('', file=sys.stderr)
-    from src.frontends.cmdline import loxodo
-    sys.exit()
-except ImportError as e:
-    print('Could not find wxPython, the wxWidgets Python bindings: %s' % e, file=sys.stderr)
-    print('Falling back to cmdline frontend.', file=sys.stderr)
-    print('', file=sys.stderr)
-    from src.frontends.cmdline import loxodo
-    sys.exit()
+def main():
+    # If cmdline arguments were given, use the "cmdline" frontend.
+    if len(sys.argv) > 1:
+        from src.frontends.cmdline import loxodo
+        sys.exit()
 
-from src.frontends.wx import loxodo
+    # In all other cases, use the "wx" frontend.
+    try:
+        import wx
+        assert wx.__version__.startswith('4.0.')
+    except AssertionError as e:
+        print('Found incompatible wxPython, the wxWidgets Python bindings: %s' % wx.__version__, file=sys.stderr)
+        print('Falling back to cmdline frontend.', file=sys.stderr)
+        print('', file=sys.stderr)
+        from src.frontends.cmdline import loxodo
+        sys.exit()
+    except ImportError as e:
+        print('Could not find wxPython, the wxWidgets Python bindings: %s' % e, file=sys.stderr)
+        print('Falling back to cmdline frontend.', file=sys.stderr)
+        print('', file=sys.stderr)
+        from src.frontends.cmdline import loxodo
+        sys.exit()
 
+    from src.frontends.wx import loxodo
+
+
+if __name__ == '__main__':
+    main()
